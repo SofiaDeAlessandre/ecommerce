36ebb26e183ae11674e1799e28a9ef459c526c6f
SofiaDeAlessandre <sofia.dealessandre@hotmail.com>
2024-07-26 12:59:16 -0300

    add FirebaseContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import './App.css'
+import { Container, Typography } from '@mui/material';
+import { useContext } from 'react';
+import { FirebaseContext } from './context/FirebaseContext';
 
 function App() {
-  
-  return (
-    <>
-      
-    </>
-  )
+const { products } = useContext(FirebaseContext)
+const { users } = useContext(FirebaseContext)
+return (
+  <Container>
+    {products.map(product => (
+      <Container key={product.id}>
+        <Typography>{product.id}</Typography>
+        <Typography>{product.descripcion}</Typography>
+        <Typography>{product.nombre}</Typography>
+        <Typography>{product.precio}</Typography>
+      </Container>
+    ))}
+    <h1>usuarios</h1>
+			<ul>
+				{users?.map((user) => (
+					<li key={user.id}>
+						<span>{user.nombre}</span>
+					</li>
+				))}
+			</ul>
+  </Container>
+);
 }
-
-export default App
+export default App;
---
+++ src/context/FirebaseContext.jsx
@@ -0,0 +1,40 @@
+import { createContext, useEffect, useState } from "react"
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+
+export const FirebaseContext = createContext()
+
+export const FirebaseProvider = ({ children }) => {
+    const [products, setProducts] = useState([])
+    const [users, setUsers] = useState([])
+
+    const getCollection = async () => {
+        const collectionReference = collection(db, "productos");
+        console.log("no-bucle");
+        const results = await getDocs(collectionReference);
+        const newArray = results.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setProducts(newArray);
+    
+    }
+    useEffect(() => {
+      getCollection();
+      console.log("no-bucle")
+    }, []);
+
+    const getCollection1 = async () => {
+        const collectionReference1 = collection(db, "usuarios");
+        console.log("no-bucle");
+        const results = await getDocs(collectionReference1);
+        const newArrayUsers = results.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        setUsers(newArrayUsers);
+    
+    }
+    useEffect(() => {
+      getCollection1();
+      console.log("no-bucle")
+    }, []);
+
+  return (
+    <FirebaseContext.Provider value={{products, setProducts, users, setUsers}}>{children}</FirebaseContext.Provider>
+  )
+}
